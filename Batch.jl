3f0283792ec4dfe2a60245f99436fd5bfde982c7
Peichao Li <peichao.c.li@gmail.com>
2020-04-10 15:48:39 -0700

    add STA

--- Batch.jl
+++ Batch.jl
@@ -1502,6 +1502,159 @@ function process_2P_dirsfcolor(files,param;uuid="",log=nothing,plot=false)
 
 end
 
+function process_2P_hartleySTA(files,param;uuid="",log=nothing,plot=false)
+
+    interpolatedData = haskey(param,:interpolatedData) ? param[:interpolatedData] : true   # If you have multiplanes. True: use interpolated data; false: use uniterpolated data. Results are slightly different.
+    delays = -0.1:0.05:0.5
+    print(collect(delays))
+
+    # Expt info
+    dataset = prepare(files)
+    ex = dataset["ex"]
+    disk=ex["source"][1:2];subject=ex["Subject_ID"];recordSession=uppercasefirst(ex["RecordSite"]);testId=ex["TestID"]
+
+    ## Prepare data & result path
+    exptId = join(filter(!isempty,[recordSession[2:end], testId]),"_")
+    dataFolder = joinpath(disk,subject, "2P_data", recordSession, exptId)
+
+    ## load expt, scanning parameters
+    envparam = ex["EnvParam"]
+    coneType = getparam(envparam,"colorspace")
+    sbx = dataset["sbx"]["info"]
+    sbxft = ex["frameTimeSer"]   # time series of sbx frame in whole recording
+    # Condition Tests
+    envparam = ex["EnvParam"];preicidur = ex["PreICI"];conddur = ex["CondDur"];suficidur = ex["SufICI"]
+    condon = ex["CondTest"]["CondOn"]
+    condoff = ex["CondTest"]["CondOff"]
+    condidx = ex["CondTest"]["CondIndex"]
+    # condtable = DataFrame(ex["Cond"])
+    condtable =  DataFrame(ex["raw"]["log"]["randlog_T1"]["domains"]["Cond"])
+    rename!(condtable, [:oridom, :kx, :ky,:bwdom,:colordom])
+    ## Load data
+    segmentFile=matchfile(Regex("[A-Za-z0-9]*[A-Za-z0-9]*_merged.segment"),dir=dataFolder,adddir=true)[1]
+    segment = prepare(segmentFile)
+    signalFile=matchfile(Regex("[A-Za-z0-9]*[A-Za-z0-9]*_merged.signals"),dir=dataFolder,adddir=true)[1]
+    signal = prepare(signalFile)
+    # sig = transpose(signal["sig"])   # 1st dimention is cell roi, 2nd is fluorescence trace
+    spks = transpose(signal["spks"])  # 1st dimention is cell roi, 2nd is spike train
+
+    ##
+    # Prepare Imageset
+    nscale = haskey(param,:nscale) ? param[:nscale] : 1
+    downsample = haskey(param,:downsample) ? param[:downsample] : 2
+    sigma = haskey(param,:sigma) ? param[:sigma] : 1.5
+    # bgRGB = [getparam(envparam,"backgroundR"),getparam(envparam,"backgroundG"),getparam(envparam,"backgroundB")]
+    bgcolor = RGBA([0.5,0.5,0.5,1]...)
+    coneType = string(getparam(envparam,"colorspace"))
+    masktype = getparam(envparam,"mask_type")
+    maskradius = getparam(envparam,"mask_radius")
+    masksigma = 1#getparam(envparam,"Sigma")
+    xsize = getparam(envparam,"x_size")
+    ysize = getparam(envparam,"y_size")
+    stisize = xsize
+    ppd = haskey(param,:ppd) ? param[:ppd] : 46
+    imagesetname = "Hartley_stisize$stisize"
+    maskradius = 0.16 #maskradius/stisize
+    # if coneType == "L"
+    #     maxcolor = RGBA()
+    #     mincolor = RGBA()
+    # elseif coneType == "M"
+    #
+    # elseif coneType == "S"
+    #
+    # end
+
+    if !haskey(param,imagesetname)
+        imageset = map(i->GrayA.(hartley(kx=i.kx,ky=i.ky,bw=i.bwdom,stisize=stisize, ppd=ppd)),eachrow(condtable))
+        # imageset = map(i->GrayA.(grating(θ=deg2rad(i.Ori),sf=i.SpatialFreq,phase=rem(i.SpatialPhase+1,1)+0.02,stisize=stisize,ppd=23)),eachrow(condtable))
+        imageset = Dict{Symbol,Any}(:pyramid => map(i->gaussian_pyramid(i, nscale-1, downsample, sigma),imageset))
+        imageset[:imagesize] = map(i->size(i),imageset[:pyramid][1])
+        param[imagesetname] = imageset
+    end
+
+    # Prepare Image Stimuli
+    imageset = param[imagesetname]
+    bgcolor = oftype(imageset[:pyramid][1][1][1],bgcolor)
+    unmaskindex = map(i->alphamask(i,radius=maskradius,sigma=masksigma,masktype=masktype)[2],imageset[:pyramid][1])
+    imagestimuli = map(s->map(i->alphablend.(alphamask(i[s],radius=maskradius,sigma=masksigma,masktype=masktype)[1],[bgcolor]),imageset[:pyramid]),1:nscale)
+
+
+    ## Load data
+    planeNum = size(segment["mask"],3)  # how many planes
+    planeStart = vcat(1, length.(segment["seg_ot"]["vert"]).+1)
+
+    ## Use for loop process each plane seperately
+    for pn in 1:planeNum
+        # pn=1  # for test
+        display("plane: $pn")
+        # Initialize DataFrame for saving results
+        recordPlane = string("00",pn-1)  # plane/depth, this notation only works for expt has less than 10 planes
+        siteId = join(filter(!isempty,[recordSession[2:end], testId, recordPlane]),"_")
+        dataExportFolder = joinpath(disk,subject, "2P_analysis", recordSession, siteId, "DataExport")
+        resultFolder = joinpath(disk,subject, "2P_analysis", recordSession, siteId, "Plots")
+        isdir(dataExportFolder) || mkpath(dataExportFolder)
+        isdir(resultFolder) || mkpath(resultFolder)
+
+        cellRoi = segment["seg_ot"]["vert"][pn]
+        cellNum = length(cellRoi)
+        display("Cell Number: $cellNum")
+
+        if interpolatedData
+            # rawF = sig[planeStart[pn]:planeStart[pn]+cellNum-1,:]
+            spike = spks[planeStart[pn]:planeStart[pn]+cellNum-1,:]
+        else
+            # rawF = transpose(signal["sig_ot"]["sig"][pn])
+            spike = transpose(signal["sig_ot"]["spks"][pn])
+        end
+
+        ## Calculate STA
+        if :STA in param[:model]
+            scaleindex=1
+            imagesize = imageset[:imagesize][scaleindex]
+            xi = unmaskindex[scaleindex]
+            uci = unique(condidx)
+            ucii = map(i->findall(condidx.==i),uci)  # find the repeats of each unique condition
+            x = Array{Float64}(undef,length(uci),length(xi))
+            foreach(i->x[i,:]=gray.(imagestimuli[scaleindex][uci[i]][xi]),1:size(x,1))
+
+            uy = Array{Float64}(undef,cellNum,length(delays),length(ucii))
+            usta = Array{Float64}(undef,cellNum,length(delays),length(xi))
+            # uy = Array{Float64}(undef,length(delays),length(ucii))
+            # usta = Array{Float64}(undef,length(delays),length(xi))
+
+            for d in eachindex(delays)
+                display("Processing delay: $d")
+                y,num,wind,idx = subrv(sbxft,condon.+delays[d], condoff.+delays[d],isminzero=false,ismaxzero=false,shift=0,israte=false)
+                spk=zeros(size(spike,1),length(idx))
+                for i =1:length(idx)
+                    spkepo = @view spike[:,idx[i][1]:idx[i][end]]
+                    spk[:,i]= mean(spkepo, dims=2)
+                end
+                for cell in 1:cellNum
+                    # display(cell)
+                    y = map(i->mean(spk[cell,:][i]),ucii)
+                    stas = sta(x,y)
+                    uy[cell,d,:]=y
+                    usta[cell,d,:]=stas
+                    # uy[d,:]=y
+                    # usta[d,:]=stas
+                    if isplot
+                        r = [extrema(stas)...]
+                        title = "$(ugs[cell])Unit_$(unitid[cell])_STA_$(delays[d])"
+                        p = plotsta(stas,imagesize=imagesize,stisize=stisize,index=xi,title=title,r=r)
+                        foreach(i->save(joinpath(resultFolder,"$title$i"),p),[".png"])
+                    end
+                end
+            end
+            # uy=DefaultDict();usta = DefaultDict()
+            save(joinpath(dataExportFolder,join([subject,"_",siteId,"_",coneType,"_sta.jld2"])),"imagesize",imagesize,"x",x,"xi",xi,"xcond",condtable[uci,:],
+            "uy",uy,"usta",usta,"delays",delays,"stisize",stisize,"color",coneType)
+        end
+    end
+
+end
+
+
 function process_2P_hartleyFourier(files,param;uuid="",log=nothing,plot=false)
 
     interpolatedData = haskey(param,:interpolatedData) ? param[:interpolatedData] : true   # If you have multiplanes. True: use interpolated data; false: use uniterpolated data. Results are slightly different.
