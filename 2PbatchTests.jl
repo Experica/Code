c27d045e28ba4c10c6ff209326050b82203fb21e
Peichao Li <peichao.c.li@gmail.com>
2020-10-06 13:19:23 -0700

    update

--- 2PbatchTests.jl
+++ 2PbatchTests.jl
@@ -13,7 +13,7 @@ param = Dict{Any,Any}(
     :diraucThres => 0.8,   # if passed, calculate hue direction, otherwise calculate hue axis
     :oriaucThres => 0.5,
     :Respthres => 0.1,  # Set a response threshold to filter out low response cells?
-    :blankId => 36,  # Blank Id; AF4=36, AE6AE7=34
+    :blankId => 36,  # Blank Id; AF3AF4=36, AE6AE7=34
     :excId => [27,28])  # Exclude some condition?
 
 meta = readmeta(joinpath(param[:dataexportroot],"metadata.mat"))
@@ -25,11 +25,11 @@ end
 tests = @from i in meta begin
         # @where startswith(get(i.Subject_ID), "AF4")
         @where i.Subject_ID == "AF4"
+        # @where i.RecordSite == "u003"
         @where i.RecordSite != "u001"
-        # @where i.RecordSite != "u004"
-        # @where i.RecordSite != "u007"
+        @where i.RecordSite != "u007"
         # @where i.RecordSite != "u010"
-        @where i.ID == "Hartley"
+        @where i.ID == "DirSFColor"
         @where i.sourceformat == "Scanbox"
         @select {i.sourceformat,i.ID,i.files,i.RecordSite,i.filename,i.UUID}
         @collect DataFrame
@@ -37,7 +37,7 @@ tests = @from i in meta begin
 
 # tests.files = replace.(tests.files, Ref("H" => string(param[:dataexportroot][1])))   # Somtime the disk name changes becasue whatever reason
 sort!(tests, [:RecordSite, :filename])
-showall(tests)
+show(tests, allrows = true, allcols = true)
 ## Condition Tests
 batchtests(tests,param,plot=false)
 
@@ -51,7 +51,7 @@ param[:delayLB] = -0.066  # in sec; Usually do not need to change it
 param[:delayUB] = 0.4   # in sec; Usually do not need to change it
 param[:delayStep] = 0.033  # Bidirectional = 0.033 sec, Unidirectional = 0.066 sec
 param[:delays] = param[:delayLB]:param[:delayStep]:param[:delayUB]
-param[:maskradius] =0.16 #AE6=0.18 AE7=0.24 AF4=0.16  # It is proportion %: mask_radius/size + 0.3
+param[:maskradius] =0.15 #AE6=0.18 AE7=0.24 AF3=0.15 AF4=0.16  # It is proportion %: mask_radius/size + 0.3
 
 param[:epprndelay]=1
 param[:epprnft]=[3]
--- 2PresultOrganizerMultiplePlane.jl
+++ 2PresultOrganizerMultiplePlane.jl
@@ -5,9 +5,9 @@
 using NeuroAnalysis,Statistics,DataFrames,DataFramesMeta,StatsPlots,Mmap,Images,StatsBase,Interact, CSV,MAT,Query,DataStructures, HypothesisTests, StatsFuns, Random
 
 # Expt info
-disk = "O:"
-subject = "AF4"  # Animal
-# recordSession = ["002","003","004", "005", "006"] # Unit
+disk = "J:"
+subject = "AF3"  # Animal
+# recordSession = ["002","003","004", "005", "006", "007", "008"] # Unit
 recordPlane = ["000", "001"]
 
 oriaucThres = 0.7
@@ -24,9 +24,9 @@ huesiThres = 0.5
 cpiThres = 0.33
 
 numberofColor = 12
-colorSpace = "DKL"   # DKL, # HSL
+colorSpace = "HSL"   # DKL, # HSL
 
-addSTA = true
+addSTA = false
 staThres = 0.25
 
 ## Make folders and path
@@ -48,26 +48,26 @@ tests = @from i in meta begin
     end
 sort!(tests, [:RecordSite, :filename])
 oritests = @from i in tests begin
-    @where i.RecordSite != "u001"
-    @where i.RecordSite != "u007"
+    # @where i.RecordSite == "u003"
+    # @where i.RecordSite != "u007"
     @where i.ID == "DirSF"
     @select {i.ID,i.RecordSite,i.filename}
     @collect DataFrame
     end
-deleterows!(oritests, [2,3,4,5,6,8,9,11,12,13,14,15,16,17,19,20,21,22,24,25,26,27])
-
+# deleterows!(oritests, [2,3,4,5,6,8,9,11,12,13,14,15,16,17,19,20,21,22,24,25,26,27])
+deleterows!(oritests, [2,4,5,8,10,12,14])
 huetests = @from i in tests begin
-    @where i.RecordSite != "u001"
-    @where i.RecordSite != "u007"
+    # @where i.RecordSite == "u003"
+    # @where i.RecordSite != "u007"
     @where i.ID == "DirSFColor"
     @select {i.ID,i.RecordSite,i.filename}
     @collect DataFrame
     end
-deleterows!(huetests, [2,3,6,7,9])
+# deleterows!(huetests, [2,3,6,7,9])
 
 hartleytests = @from i in tests begin
-    @where i.RecordSite != "u001"
-    @where i.RecordSite != "u007"
+    # @where i.RecordSite == "u003"
+    # @where i.RecordSite != "u007"
     @where i.ID == "Hartley"
     @select {i.ID,i.RecordSite,i.filename}
     @collect DataFrame
@@ -100,11 +100,11 @@ for i = 1:exptOriNum
         siteId = join([unitId, "_",recordPlane[j]])
         dataFolder = joinpath(mainpath, join(["U", unitId]), join([oriExptId[i][5:11], "_", recordPlane[j]]), "DataExport")
         dataFile=matchfile(Regex("[A-Za-z0-9]*[A-Za-z0-9]*_[A-Za-z0-9]*_result.jld2"),dir=dataFolder,join=true)[1]
-        # segmentFile=matchfile(Regex("[A-Za-z0-9]*[A-Za-z0-9]*_ot_[A-Za-z0-9]*.segment"),dir=dataFolder,join=true)[1]
-        # alignFile=matchfile(Regex("[A-Za-z0-9]*[A-Za-z0-9]*_ot_[A-Za-z0-9]*.align"),dir=dataFolder,join=true)[1]
+        segmentFile=matchfile(Regex("[A-Za-z0-9]*[A-Za-z0-9]*_ot_[A-Za-z0-9]*.segment"),dir=dataFolder,join=true)[1]
+        alignFile=matchfile(Regex("[A-Za-z0-9]*[A-Za-z0-9]*_ot_[A-Za-z0-9]*.align"),dir=dataFolder,join=true)[1]
 
-        segmentFile=matchfile(Regex("[A-Za-z0-9]*[A-Za-z0-9]*_[A-Za-z0-9]*.segment"),dir=dataFolder,join=true)[1]
-        alignFile=matchfile(Regex("[A-Za-z0-9]*[A-Za-z0-9]*_[A-Za-z0-9]*.align"),dir=dataFolder,join=true)[1]
+        # segmentFile=matchfile(Regex("[A-Za-z0-9]*[A-Za-z0-9]*_[A-Za-z0-9]*.segment"),dir=dataFolder,join=true)[1]
+        # alignFile=matchfile(Regex("[A-Za-z0-9]*[A-Za-z0-9]*_[A-Za-z0-9]*.align"),dir=dataFolder,join=true)[1]
 
         result = load(dataFile)["result"]
         segment = prepare(segmentFile)
@@ -117,8 +117,8 @@ for i = 1:exptOriNum
         push!(rois, cellRoi)
         push!(bkgs, bkgImg)
         summ=DataFrame(id=result.dataId[1], exptid=oriExptId[i], planeid=recordPlane[j], cellNum=length(cellRoi), visResp=sum(result.visResp),
-             oriResp=sum((result.oriauc.>oriaucThres).&result.visResp), oriRespfit=sum((result.oriauc.>oriaucThres).&result.visResp.&(.~isnan.(result.fitori))), oriSelcv=sum((result.oricv.<oricvThres).&(result.oriauc.>oriaucThres).&result.visResp), oriSelosi=sum((result.osi.>osiThres).&(result.oriauc.>oriaucThres).&result.visResp),
-             dirResp=sum((result.dirauc.>diraucThres).&result.visResp), dirRespfit=sum((result.dirauc.>diraucThres).&result.visResp.&(.~isnan.(result.fitdir))), dirSelcv=sum((result.dircv.<dircvThres).&(result.dirauc.>diraucThres).&result.visResp), dirSeldsi=sum((result.dsi.>dsiThres).&(result.dirauc.>diraucThres).&result.visResp))
+             oriResp=sum((result.oriauc.>oriaucThres).&result.visResp), oriRespfit=sum((result.oriauc.>oriaucThres).&result.visResp.&(.~isnan.(result.fitori))), oriSelcv=sum((result.oricv.<oricvThres).&(result.oriauc.>oriaucThres).&result.visResp), oriSelosi=sum((result.osi1.>osiThres).&(result.oriauc.>oriaucThres).&result.visResp),
+             dirResp=sum((result.dirauc.>diraucThres).&result.visResp), dirRespfit=sum((result.dirauc.>diraucThres).&result.visResp.&(.~isnan.(result.fitdir))), dirSelcv=sum((result.dircv.<dircvThres).&(result.dirauc.>diraucThres).&result.visResp), dirSeldsi=sum((result.dsi1.>dsiThres).&(result.dirauc.>diraucThres).&result.visResp))
         append!(oriSum, summ)
         dataExportFolder2 = joinpath(mainpath, join(["U", unitId]), "_Summary", "DataExport")
         isdir(dataExportFolder2) || mkpath(dataExportFolder2)
@@ -142,17 +142,17 @@ for i = 1:exptHueNum
         result = load(dataFile)["result"]
         locID = repeat([siteId],size(result)[1])
         insertcols!(result,4,locId=locID)
-        hueResp = 1 .- minimum(hcat(result.hueaxcv, result.huedicv),dims=2)
-        achroResp = 1 .- resultori.oricv
-        cpi = dropdims((hueResp .- achroResp) ./ (hueResp .+ achroResp),dims=2)
-        insertcols!(result,11,huecpi=cpi)
+        # hueResp = 1 .- minimum(hcat(result.hueaxcv, result.huedicv),dims=2)
+        # achroResp = 1 .- resultori.oricv
+        # cpi = dropdims((hueResp .- achroResp) ./ (hueResp .+ achroResp),dims=2)
+        # insertcols!(result,11,huecpi=cpi)
         append!(hueData, result)
         summ=DataFrame(id=result.dataId[1], exptid=hueExptId[i], planeid=recordPlane[j], cellNum=size(result)[1], visResp=sum(result.visResp),
-             hueaxResp=sum((result.hueaxauc.>hueaucThres).&result.visResp), hueaxRespfit=sum((result.hueaxauc.>hueaucThres).&result.visResp.&(.~isnan.(result.fithueax))),hueaxSelcv=sum((result.hueaxcv.<huecvThres).&(result.hueaxauc.>hueaucThres).&result.visResp), hueaxSelsi=sum((result.hueaxsi.>huesiThres).&(result.hueaxauc.>hueaucThres).&result.visResp),
-             huediResp=sum((result.huediauc.>hueaucThres).&result.visResp), huediRespfit=sum((result.huediauc.>hueaucThres).&result.visResp.&(.~isnan.(result.fithuedi))),huediSelcv=sum((result.huedicv.<huecvThres).&(result.huediauc.>hueaucThres).&result.visResp), huediSelsi=sum((result.huedisi.>huesiThres).&(result.huediauc.>hueaucThres).&result.visResp),
-             hueoriResp=sum((result.hueoriauc.>oriaucThres).&result.visResp), hueoriRespfit=sum((result.hueoriauc.>oriaucThres).&result.visResp.&(.~isnan.(result.fithueori))),hueoriSelcv=sum((result.hueoricv.<oricvThres).&(result.hueoriauc.>oriaucThres).&result.visResp), hueoriSeldsi=sum((result.hueosi.>osiThres).&(result.hueoriauc.>oriaucThres).&result.visResp),
-             huedirResp=sum((result.huedirauc.>diraucThres).&result.visResp), huedirRespfit=sum((result.huedirauc.>diraucThres).&result.visResp.&(.~isnan.(result.fithuedir))),huedirSelcv=sum((result.huedircv.<dircvThres).&(result.huedirauc.>diraucThres).&result.visResp), huedirSeldsi=sum((result.huedsi.>dsiThres).&(result.huedirauc.>diraucThres).&result.visResp),
-             hueaucResp=sum(((result.hueaxauc.>hueaucThres).|(result.huediauc.>hueaucThres)).&result.visResp), hueSelcpi=sum(((result.hueaxauc.>hueaucThres).|(result.huediauc.>hueaucThres)).&result.visResp.&(cpi.>cpiThres)))
+             hueaxResp=sum((result.hueaxauc.>hueaucThres).&result.visResp), hueaxRespfit=sum((result.hueaxauc.>hueaucThres).&result.visResp.&(.~isnan.(result.fithueax))),hueaxSelcv=sum((result.hueaxcv.<huecvThres).&(result.hueaxauc.>hueaucThres).&result.visResp), hueaxSelsi=sum((result.hueaxsi1.>huesiThres).&(result.hueaxauc.>hueaucThres).&result.visResp),
+             huediResp=sum((result.huediauc.>hueaucThres).&result.visResp), huediRespfit=sum((result.huediauc.>hueaucThres).&result.visResp.&(.~isnan.(result.fithuedi))),huediSelcv=sum((result.huedicv.<huecvThres).&(result.huediauc.>hueaucThres).&result.visResp), huediSelsi=sum((result.huedisi1.>huesiThres).&(result.huediauc.>hueaucThres).&result.visResp),
+             hueoriResp=sum((result.hueoriauc.>oriaucThres).&result.visResp), hueoriRespfit=sum((result.hueoriauc.>oriaucThres).&result.visResp.&(.~isnan.(result.fithueori))),hueoriSelcv=sum((result.hueoricv.<oricvThres).&(result.hueoriauc.>oriaucThres).&result.visResp), hueoriSeldsi=sum((result.hueosi1.>osiThres).&(result.hueoriauc.>oriaucThres).&result.visResp),
+             huedirResp=sum((result.huedirauc.>diraucThres).&result.visResp), huedirRespfit=sum((result.huedirauc.>diraucThres).&result.visResp.&(.~isnan.(result.fithuedir))),huedirSelcv=sum((result.huedircv.<dircvThres).&(result.huedirauc.>diraucThres).&result.visResp), huedirSeldsi=sum((result.huedsi1.>dsiThres).&(result.huedirauc.>diraucThres).&result.visResp),
+             hueaucResp=sum(((result.hueaxauc.>hueaucThres).|(result.huediauc.>hueaucThres)).&result.visResp))#, hueSelcpi=sum(((result.hueaxauc.>hueaucThres).|(result.huediauc.>hueaucThres)).&result.visResp.&(cpi.>cpiThres)))
         append!(hueSum, summ)
     end
 end
@@ -318,8 +318,8 @@ for i = 1:sessNum
             pldata2.modulative = pldata1.modulative + pldata2.modulative
             pldata = hcat(pldata2, select(pldata1, Not([:py,:ani,:dataId,:locId,:cellId,:visResp,:responsive,:modulative])))
             planesum=DataFrame(session=recordSession[i], planeid=recordPlane[j], cellNum=size(pldata)[1], visResp=sum(pldata.visResp.>0),
-                oriResp=sum((pldata.oriauc.>oriaucThres).&pldata.visResp), oriRespfit=sum((pldata.oriauc.>oriaucThres).&pldata.visResp.&(.~isnan.(pldata.fitori))), oriSelcv=sum((pldata.oricv.<oricvThres).&(pldata.oriauc.>oriaucThres).&pldata.visResp), oriSelosi=sum((pldata.osi.>osiThres).&(pldata.oriauc.>oriaucThres).&pldata.visResp),
-                dirResp=sum((pldata.dirauc.>diraucThres).&pldata.visResp), dirRespfit=sum((pldata.dirauc.>diraucThres).&pldata.visResp.&(.~isnan.(pldata.fitdir))), dirSelcv=sum((pldata.dircv.<dircvThres).&(pldata.dirauc.>diraucThres).&pldata.visResp), dirSeldsi=sum((pldata.dsi.>dsiThres).&(pldata.dirauc.>diraucThres).&pldata.visResp),
+                oriResp=sum((pldata.oriauc.>oriaucThres).&pldata.visResp), oriRespfit=sum((pldata.oriauc.>oriaucThres).&pldata.visResp.&(.~isnan.(pldata.fitori))), oriSelcv=sum((pldata.oricv.<oricvThres).&(pldata.oriauc.>oriaucThres).&pldata.visResp), oriSelosi=sum((pldata.osi1.>osiThres).&(pldata.oriauc.>oriaucThres).&pldata.visResp),
+                dirResp=sum((pldata.dirauc.>diraucThres).&pldata.visResp), dirRespfit=sum((pldata.dirauc.>diraucThres).&pldata.visResp.&(.~isnan.(pldata.fitdir))), dirSelcv=sum((pldata.dircv.<dircvThres).&(pldata.dirauc.>diraucThres).&pldata.visResp), dirSeldsi=sum((pldata.dsi1.>dsiThres).&(pldata.dirauc.>diraucThres).&pldata.visResp),
                 hueaxResp=sum((pldata.hueaxauc.>hueaucThres).&pldata.visResp), hueaxRespfit=sum((pldata.hueaxauc.>hueaucThres).&pldata.visResp.&(.~isnan.(pldata.fithueax))),hueaxSelcv=sum((pldata.hueaxcv.<huecvThres).&(pldata.hueaxauc.>hueaucThres).&pldata.visResp), hueaxSelsi=sum((pldata.hueaxsi.>huesiThres).&(pldata.hueaxauc.>hueaucThres).&pldata.visResp),
                 huediResp=sum((pldata.huediauc.>hueaucThres).&pldata.visResp), huediRespfit=sum((pldata.huediauc.>hueaucThres).&pldata.visResp.&(.~isnan.(pldata.fithuedi))),huediSelcv=sum((pldata.huedicv.<huecvThres).&(pldata.huediauc.>hueaucThres).&pldata.visResp), huediSelsi=sum((pldata.huedisi.>huesiThres).&(pldata.huediauc.>hueaucThres).&pldata.visResp),
                 hueoriResp=sum((pldata.hueoriauc.>oriaucThres).&pldata.visResp), hueoriRespfit=sum((pldata.hueoriauc.>oriaucThres).&pldata.visResp.&(.~isnan.(pldata.fithueori))),hueoriSelcv=sum((pldata.hueoricv.<oricvThres).&(pldata.hueoriauc.>oriaucThres).&pldata.visResp), hueoriSeldsi=sum((pldata.hueosi.>osiThres).&(pldata.hueoriauc.>oriaucThres).&pldata.visResp),
